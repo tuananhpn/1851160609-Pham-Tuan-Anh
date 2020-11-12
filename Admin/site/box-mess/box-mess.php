72a8e16b62b3446ff87658654ed439f977a762fc
daovanhai <1851161809@e.tlu.edu.vn>
2020-11-12 19:32:32 +0700

    frontend

---
+++ Admin/site/box-mess/box-mess.php
@@ -0,0 +1,64 @@
+<!-- Box-mess -->
+
+<div id="box_mess" class="position-fixed" style="bottom:20px; right:40px;">
+                <div class="header">
+                    <div class="top_header">
+                        <div id="title">CỘNG STUDIO</div>
+                        <div id="dong" title="dong"><a href="javascript:box_mess()">X</a></div>
+                    </div>
+                    <div class="bottom_header">
+                        <div><img src="images/fiqsvfdocrzauckgtvbf-111111.jpg" alt=""></div>
+                        <div>
+                            <div class="right">Cộng Studio sẵn sàng hỗ trợ ! Vui lòng gửi yêu cầu để được tư vấn nhanh nhất về
+                                các dịch vụ tại Cộng Studio.</div>
+                            <div class="social-link">
+                                <a href=""><i class="fa fa-envelope" aria-hidden="true"></i></a>
+                            </div>
+                        </div>
+                    </div>
+        
+                </div>
+                <div class="convo-list">
+                    <div class="convo-container">
+                        <div class="convo-title">
+                            <div>Danh Sách Hội Thoại</div>
+                        </div>
+                        <div class="item">
+                            <a href="#">
+                            <div id="item-c">
+                                
+                                <div class="avatar"><img src="images/fiqqytdimhfdqsdlomlz-cong_studio_final_05.jpg" alt=""></div>
+                                <div class="mess">
+                                    <div>
+                                        <div class="convo-name">CỘNG STUDIO</div>
+                                        <div class="convo-time">3 giờ trước</div>
+                                    </div>
+                                    <div>Đã gửi cho bạn một tin nhắn</div>
+                                </div>
+                            </div> </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="convo-footer">
+                    <button class="start-convo">
+                        <span>Tạo hội thoại mới</span>
+                        <img src="https://public-gcs.subiz-cdn.com/widget-v4/public/img/send.c4dc0cd3.svg" alt="true">
+                    </button>
+                    <div class="pow">
+                        <a href="#">Phát triển bởi
+                            <img src="https://public-gcs.subiz-cdn.com/widget-v4/public/img/subiz-mono.0828dc90.svg" alt="true"
+                                class="poweredby--subiz-logo">
+                            Subiz
+                        </a>
+                    </div>
+                </div>
+        
+            </div>
+        
+            <div id="img" class="position-fixed" style="bottom:10px; right:5px;">
+        
+                <a href="javascript:box_mess()"><img src="images/bubble_lib_01 (1).svg" width="200"></a>
+        
+            </div>
+
+            <!-- End Box-mess -->
